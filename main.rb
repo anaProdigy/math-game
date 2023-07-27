68e84e91e6d36ac811df242974648ad011fc1718
Anastasia <z.anastasia11@gmail.com>
2023-07-26 18:11:26 -0700

    add turn class

--- main.rb
+++ main.rb
@@ -2,7 +2,27 @@ require "./player.rb"
 require './question.rb'
 require './game.rb'
 
-# Create a new game instance
+# class Test
+#   def initialize
+#   self.prompt
+#   self.capture
+   
+#   end
+
+#   def prompt
+#       puts "Hello there"
+#   end
+
+#   def capture
+#     gets.chomp
+#   end
+
+# end
+
+# Test.new
+
+
+#Create a new game instance
 game = Game.new
 
 
@@ -12,5 +32,7 @@ while game.in_progress
   #start playing turn logic
   game.start_turn
  # check if anybody won if yes stop game
+
 end
-puts game.in_progress
+
+# puts game.in_progress
--- turn.rb
+++ turn.rb
@@ -1,15 +1,46 @@
 class Turn 
 
-  attr_accessor :players, :question, :round
+  attr_accessor :players, :question, :round, :current_player, :next_player 
 
   def initialize(players, question, round)
     @players = players
-    @question = Question.new
-    puts "#{starter_player} #{question}"
+    @question = question
+    @round = round
+    
+
+    if players[0].turn == true
+      @current_player = players[0]
+      @next_player = players[1]
+    else
+      @current_player = players[1]
+      @next_player = players[0]
+    end
+
+    self.ask_question
+    self.compare_answers
+
     # ask
     # check
     # maintain lifes
     # reduce life
     # game ove?
+  end
+
+  def ask_question 
+    puts "-- Round ##{round} --"
+    puts "#{self.current_player.name} #{self.question}"
+  end
+
+  def compare_answers
+   player_answer = gets.chomp.to_i
+   if self.question.answer == player_answer
+     puts "Great job! Correct answer"
+   else
+    self.current_player.lives -= 1
+    puts "YOU SUCK!"
+   end
+   self.current_player.turn = false
+   self.next_player = true
+   puts "Player1 #{self.players[0].name} has #{self.players[0].lives}/3 VS Player2 #{self.players[1].name} has #{self.players[1].lives}/3"
   end
 end
